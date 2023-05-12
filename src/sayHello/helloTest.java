d44c313f7bfa8c531c1964bb71e9540af10d7122
Cameron Swartzell <csswartzell@gmail.com>
2023-05-11 20:11:07 -0700

    just commit right?

--- src/sayHello/helloTest.java
+++ src/sayHello/helloTest.java
@@ -2,6 +2,7 @@ package sayHello;
 public class helloTest {
 	public static void main(String args[]) {
 //		adding a comment to test shortcuts and git
+//		updating is just git commit -m "message here right?" 
 		System.out.println("Hello World!");
 	}
 }
