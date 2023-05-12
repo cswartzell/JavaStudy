1f2a0aaee484189a82e2b42b84f58a67654d4c55
Cameron Swartzell <csswartzell@gmail.com>
2023-05-11 20:13:34 -0700

    once more

--- src/sayHello/helloTest.java
+++ src/sayHello/helloTest.java
@@ -3,6 +3,7 @@ public class helloTest {
 	public static void main(String args[]) {
 //		adding a comment to test shortcuts and git
 //		updating is just git commit -m "message here right?" 
+//		Still confused... Save. git add .  git commit -m "message". Do I need to push?
 		System.out.println("Hello World!");
 	}
 }
